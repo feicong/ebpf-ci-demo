1968915561c1cb298470ab8884ccfea89520fe57
fei_cong <fei_cong@hotmail.com>
2023-03-11 17:06:14 +0800

    patch ebpf for 5.4 arm64

--- compile-arm64-common-kernel-5.4.sh
+++ compile-arm64-common-kernel-5.4.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/env bash
 # https://github.com/nathanchance/android-kernel-clang
 # https://source.android.google.cn/setup/build/building-kernels
 # https://android.googlesource.com/kernel/manifest/+/refs/heads/common-android11-5.4/default.xml
@@ -35,9 +35,11 @@ rm -rf  prebuilts-master/clang/host/linux-x86/clang-r35*
 rm -rf  prebuilts-master/clang/host/linux-x86/clang-r37*
 
 echo patching code.
-cd common
 
-git checkout android11-5.4
+rm -rf common
+git clone https://github.com/feicong/aosp_android_common_kernels common
+cd common
+git checkout android-11-5.4-bpf_probe_read_user
 git rev-parse HEAD
 git status || true
 
